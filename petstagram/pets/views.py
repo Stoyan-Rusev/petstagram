d8a8f686c14fb49ce3155678a81c8c23235ea30a
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-04-11 16:52:44 +0300

    feat: Implemented simple pet-detail functionallity

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from petstagram.pets.models import Pet
+
 
 # Create your views here.
 def pet_add_page(request):
@@ -15,4 +17,10 @@ def pet_edit_page(request, username: str, pet_slug: str):
 
 
 def pet_details_page(request, username: str, pet_slug: str):
-    return render(request, 'pets/pet-details-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+
+    context = {
+        'pet': pet,
+    }
+
+    return render(request, 'pets/pet-details-page.html', context)
