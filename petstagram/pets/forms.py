49e0686c348e9ab61dd814d3fd9940f047af4e26
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-04-15 22:55:38 +0300

    feat: Added widgets to the PetBaseForm for better UI

--- petstagram/pets/forms.py
+++ petstagram/pets/forms.py
@@ -3,8 +3,31 @@ from django import forms
 from petstagram.pets.models import Pet
 
 
-class PetAddForm(forms.ModelForm):
+class PetBaseForm(forms.ModelForm):
     class Meta:
         model = Pet
         fields = ['name', 'personal_photo', 'date_of_birth']
 
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Pet name'}),
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+            'personal_photo': forms.TextInput(attrs={'placeholder': 'Add Image Url'})
+        }
+
+        labels = {
+            'name': 'Pet Name',
+            'date_of_birth': 'Date of Birth',
+            'personal_photo': 'Link to Image',
+        }
+
+
+class PetAddForm(PetBaseForm):
+    pass
+
+
+class PetEditForm(PetBaseForm):
+    pass
+
+
+class PetDeleteForm(PetBaseForm):
+    pass
