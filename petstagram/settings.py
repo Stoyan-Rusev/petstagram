b502183ada687fa3da506ed78b9e4bab06dc7db2
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-04-17 00:00:56 +0300

    feat: Visualised images in browser when DEBUG=True

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -131,8 +131,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_URL = 'mediafiles/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
