894c9835feab09c2f0ce1db75762c4dd2ff3a215
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-04-17 11:22:21 +0300

    feat: Implemented simple add photo functionality

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from petstagram.photos.forms import PhotoCreateForm
 from petstagram.photos.models import Photo
 
 
 # Create your views here.
 def add_photo(request):
-    return render(request, 'photos/photo-add-page.html')
+    form = PhotoCreateForm(request.POST or None, request.FILES or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('home-page')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'photos/photo-add-page.html', context=context)
 
 
 def photo_details(request, pk: int):
