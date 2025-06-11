3bca7dad3bbb3a90685afc36cb91cf156fe30cf4
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-06-11 16:58:49 +0300

    feat: implement custom register functionallity with automatic login

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,13 +1,31 @@
+from django.contrib.auth import get_user_model, login
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from petstagram.accounts.forms import AppUserCreationForm
+
+UserModel = get_user_model()
 
 
-# Create your views here.
+class AppUserRegisterView(CreateView):
+    model = UserModel
+    form_class = AppUserCreationForm
+    template_name = 'accounts/register-page.html'
+    success_url = reverse_lazy('home-page')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
+
+
 def register(request):
     return render(request, 'accounts/register-page.html')
 
 
-def login(request):
-    return render(request, 'accounts/login-page.html')
+# def login(request):
+#     return render(request, 'accounts/login-page.html')
 
 
 def show_profile_details(request, pk):
